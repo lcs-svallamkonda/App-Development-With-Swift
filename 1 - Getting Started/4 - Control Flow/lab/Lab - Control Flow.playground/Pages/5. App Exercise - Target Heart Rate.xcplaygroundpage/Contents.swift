00c0aa62aa54273d44dac6114322b8934607e95b
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-10-20 12:37:11 -0400

    Finished App Exercise - Target Heart Rate

--- 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/5. App Exercise - Target Heart Rate.xcplaygroundpage/Contents.swift
@@ -11,5 +11,11 @@ let targetLowerBound = 120
 let targetUpperBound = 150
 let currentHR = 147
 
-
+if currentHR < targetLowerBound {
+    print("You're doing great, but try to push it a bit!")
+} else if currentHR > targetUpperBound{
+    print("You're on fire! Slow it down just a bit.")
+} else {
+    print("You're right on track!")
+}
 //: [Previous](@previous)  |  page 5 of 9  |  [Next: Exercise - Switch Statements](@next)
