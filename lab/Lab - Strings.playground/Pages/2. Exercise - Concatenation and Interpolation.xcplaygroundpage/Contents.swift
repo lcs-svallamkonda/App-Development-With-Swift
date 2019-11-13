c0c8f32abbdb1f5e2d70d1877e45943e342dfab2
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-11-13 11:17:35 -0500

    added example

--- lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
+++ lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
@@ -3,15 +3,20 @@
  
  Create a `city` constant and assign it a string literal representing your home city. Then create a `state` constant and assign it a string literal representing your home state. Finally, create a `home` constant and use string concatenation to assign it a string representing your home city and state (i.e. Portland, Oregon). Print the value of `home`.
  */
-let city = " London"
-let state = ", Ontario"
-let home = city + state
+let city = "London"
+let state = "Ontario"
+let home = city + ", " + state
 /*:
  Use the compound assignment operator (`+=`) to add `home` to `introduction` below. Print the value of `introduction`.
  */
 var introduction = "I live in"
-introduction += home
+introduction += " " + home
 print(introduction)
+
+//Example: when concatanation is not enough
+let declaration = "My favourite number is"
+let number = 10
+let sentence = declaration + number
 /*:
  Declare a `name` constant and assign it your name as a string literal. Then declare an `age` constant and give it your current age as an `Int`. Then print the following phrase using string interpolation:
  
